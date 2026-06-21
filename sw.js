21221ba43c8eef71f95e65606ce0f6e96a5b77ee
scp901th-bit <scp901th@gmail.com>
2026-06-21 23:42:17 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME='bmnote-v11';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(n=>caches.delete(n)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{if(e.request.url.includes('firestore')||e.request.url.includes('googleapis')||e.request.url.includes('firebase'))return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
